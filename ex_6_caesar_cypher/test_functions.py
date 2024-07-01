7c4acd76881ac260ef70ca03f8c30cb9e846cde9
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-01 18:43:29 +0200

    tests properly adjusted

--- ex_6_caesar_cypher/test_functions.py
+++ ex_6_caesar_cypher/test_functions.py
@@ -2,15 +2,15 @@ from functions import createCypher
 
 def test_cypher_basic():
     caesar3 = createCypher(3)
-    assert caesar3("ZigZag") == "CLJCLJ"
+    assert caesar3("ZigZag") == "BLJBDJ"
 
 def test_cypher_negative_displacement():
     caesar_minus2 = createCypher(-2)
-    assert caesar_minus2("ALOHAMORA") == "YILEYJLOY"
+    assert caesar_minus2("ALOHAMORA") == "ZJNFZKNPZ"
 
 def test_cypher_spaces():
     caesar3 = createCypher(3)
-    assert caesar3("Pancho Villa") == "SDQFKRCYLÑÑD"
+    assert caesar3("Pancho Villa") == "SDPFKRCYLÑÑD"
 
 if __name__ == "__main__":
     import pytest
